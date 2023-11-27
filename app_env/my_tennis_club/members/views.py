4870b2aa9c3c48f7e06476e52c048e33f6fc4818
Bauyrzhan Yerdaliyev <baurzhanerdaliev@gmail.com>
2023-11-26 20:59:55 -0500

    all().values() method

--- app_env/my_tennis_club/members/views.py
+++ app_env/my_tennis_club/members/views.py
@@ -27,7 +27,7 @@ def main(request):
 
 
 def testing(request):
-    mydata = Member.objects.all()
+    mydata = Member.objects.all().values()
     template = loader.get_template('template.html')
     context = {
         'mymembers': mydata,
